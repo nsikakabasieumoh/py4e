8699ca0591c1504de21fec6a705d96806ec35593
Umoh Nsikakabasi E <91345422+NsikakabasiUmoh@users.noreply.github.com>
2023-03-22 13:01:26 -0400

    extracting information from a string

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from topics import functions as func
+# from topics import functions as func
 
-print(func.largest_number([3, 41, 12, 9, 74, 15]))
+# Working with basic functions
+# print(elevatorConv())
+# print(sample_try_except())
+# print(func.largest_number([3, 41, 12, 9, 74, 15]))
+
+# Examples of extract information from a string
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+atsign_postion = data.find('@')
+space_position = data.find(' ', atsign_postion)
+print(data[atsign_postion+1: space_position])
