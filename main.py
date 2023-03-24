e897aded38eb7171673d8f85f62e02ad71eabcff
Umoh Nsikakabasi E <91345422+NsikakabasiUmoh@users.noreply.github.com>
2023-03-23 20:21:16 -0400

    Search through file

--- main.py
+++ main.py
@@ -4,4 +4,11 @@
 data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
 atsign_postion = data.find('@')
 space_position = data.find(' ', atsign_postion)
-print(data[atsign_postion+1: space_position])
+# you can print 'uct.ac.za' use data[atsign_postion+1: space_position])
+
+# Search through file
+file_name = 'files/mbox-short.txt'
+data = open(file_name)
+for line in data:
+    if line.startswith('From:'):
+        print(line.strip())
